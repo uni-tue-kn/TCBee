358e7e2cefbd2be5192b9ccfabc946d4332a3bc7
Moritz Flüchter <moritz.fl@arcor.de>
2025-02-23 16:51:35 +0100

    Refactor rate calculation for UI

--- tcbee/tcbee/src/util/ebpf_watcher.rs
+++ tcbee/tcbee/src/util/ebpf_watcher.rs
@@ -1,9 +1,22 @@
-use std::{error::Error, io::{self, Write}, thread::sleep, time::{Duration, Instant}};
+use std::{
+    error::Error,
+    io::{self, Write},
+    ops::{AddAssign, Sub},
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
-use aya::{maps::PerCpuArray, util::nr_cpus};
+use aya::{maps::PerCpuArray, util::nr_cpus, Pod};
 use log::{error, info};
 use ratatui::{
-    buffer::Buffer, crossterm::event::{self, KeyCode, KeyEventKind}, layout::{Constraint, Direction, Layout, Rect}, style::{Color, Modifier, Style}, symbols, text::Span, widgets::{Axis, Block, Borders, Chart, Dataset, GraphType, Paragraph, Widget}, DefaultTerminal
+    buffer::Buffer,
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Modifier, Style},
+    symbols,
+    text::Span,
+    widgets::{Axis, Block, Borders, Chart, Dataset, GraphType, Paragraph, Widget},
+    DefaultTerminal,
 };
 use tokio_util::sync::CancellationToken;
 
@@ -11,16 +24,101 @@ enum Counter {
     Dropped,
     Handled,
     Ingress,
-    Egress
+    Egress,
+}
+
+// TODO: make more generic to handle float maps as well?
+pub struct RateWatcher<T: Pod + AddAssign + Sub> {
+    map: PerCpuArray<aya::maps::MapData, T>,
+    suffix: String,
+    last_val: T,
+    name: String,
+}
+
+impl<T: Pod + AddAssign + Default + Sub> RateWatcher<T> {
+    pub fn new(
+        map: PerCpuArray<aya::maps::MapData, T>,
+        suffix: String,
+        init_val: T,
+        name: String,
+    ) -> RateWatcher<T> {
+        RateWatcher {
+            map: map,
+            suffix: suffix,
+            last_val: init_val,
+            name: name,
+        }
+    }
+    pub fn get_rate_string(&mut self, elapsed: Duration) -> String
+    where
+        f64: From<<T as Sub>::Output>,
+    {
+        let rate = self.get_rate(elapsed);
+        RateWatcher::<T>::format_rate(rate, &self.suffix)
+    }
+
+    pub fn get_rate(&mut self, elapsed: Duration) -> f64
+    where
+        f64: From<<T as Sub>::Output>,
+    {
+        let sum = self.get_counter_sum();
+
+        // TODO: better handling?
+        let rate = f64::try_from(sum - self.last_val).unwrap_or_else(|_| -1.0)
+            * (1.0 / elapsed.as_secs_f64());
+
+        self.last_val = sum;
+
+        rate
+    }
+
+    pub fn get_counter_sum(&self) -> T {
+        // Counter is per CPU, sum will hold sum across CPUs
+        let mut sum: T = T::default();
+
+        // Get counter array
+        let values = self.map.get(&0, 0);
+
+        // Check if error erturned
+        match values {
+            Err(err) => {
+                error!("Failed to read event counter {}: {}!", self.name, err);
+            }
+            Ok(counters) => {
+                // Iterate and sum over array
+                if let Ok(num_cpus) = nr_cpus() {
+                    for i in 0..=num_cpus {
+                        sum += counters[i];
+                    }
+                } else {
+                    error!("Failed to get number of CPUs for {}", self.name);
+                }
+            }
+        }
+        sum
+    }
+
+    // TODO: prettier?
+    fn format_rate(val: f64, suffix: &str) -> String {
+        if val > 1_000_000_000.0 {
+            return format!("{} G{}", val, suffix);
+        } else if val > 1_000_000.0 {
+            return format!("{} M{}", val, suffix);
+        } else if val > 1_0000.0 {
+            return format!("{} K{}", val, suffix);
+        } else {
+            return format!("{} {}", val, suffix);
+        }
+    }
 }
 
 pub struct EBPFWatcher {
-    events_drops: PerCpuArray<aya::maps::MapData, u32>,
-    events_handled: PerCpuArray<aya::maps::MapData, u32>,
-    ingress_counter: PerCpuArray<aya::maps::MapData, u32>,
-    egress_counter: PerCpuArray<aya::maps::MapData, u32>,
+    events_drops: RateWatcher<u32>,
+    events_handled: RateWatcher<u32>,
+    ingress_counter: RateWatcher<u32>,
+    egress_counter: RateWatcher<u32>,
     token: CancellationToken,
-    terminal: Option<DefaultTerminal>
+    terminal: Option<DefaultTerminal>,
 }
 
 //TODO: Monitor packet rate vs TCP packet rate?
@@ -31,8 +129,34 @@ impl EBPFWatcher {
         ingress_counter: PerCpuArray<aya::maps::MapData, u32>,
         egress_counter: PerCpuArray<aya::maps::MapData, u32>,
         token: CancellationToken,
-        do_tui: bool
+        do_tui: bool,
     ) -> Result<EBPFWatcher, Box<dyn Error>> {
+        // Track rate of passed maps
+        let events_drops = RateWatcher::<u32>::new(
+            events_drops,
+            "Events/s".to_string(),
+            0,
+            "Event Drops".to_string(),
+        );
+        let events_handled = RateWatcher::<u32>::new(
+            events_handled,
+            "Events/s".to_string(),
+            0,
+            "Event Handled".to_string(),
+        );
+        let ingress_counter = RateWatcher::<u32>::new(
+            ingress_counter,
+            "pps".to_string(),
+            0,
+            "Ingress Packets".to_string(),
+        );
+        let egress_counter = RateWatcher::<u32>::new(
+            egress_counter,
+            "pps".to_string(),
+            0,
+            "Egress Packets".to_string(),
+        );
+
         if do_tui {
             color_eyre::install()?;
             let terminal: DefaultTerminal = ratatui::init();
@@ -54,65 +178,41 @@ impl EBPFWatcher {
                 token,
                 terminal: None,
             })
-        }        
+        }
     }
 
     pub fn run_no_tui(&mut self) {
         // To calculate rate over multiple iterations
-        let mut last_dropped: u32 = 0;
-        let mut last_handled: u32 = 0;
-        let mut last_ingress: u32 = 0;
-        let mut last_egress: u32 = 0;
-        let mut max_rate: f64 = 0.0;
-
         let application_start = Instant::now();
 
-        let mut ingress_rates: Vec<(f64, f64)> = vec![(0.0,0.0)];
-
         while !self.token.is_cancelled() {
-            // Get current counter values
-            let dropped = self.get_counter_sum(Counter::Dropped);
-            let handled = self.get_counter_sum(Counter::Handled);
-            let ingress = self.get_counter_sum(Counter::Ingress);
-            let egress = self.get_counter_sum(Counter::Egress);
-
-            // Calculate current rate in 1/s
-            // TODO: change to be based on loop sleep duration
-            let drate = (dropped - last_dropped) * 2;
-            let hrate = (handled - last_handled) * 2;
-            let irate = ((ingress - last_ingress) * 2) as f64;
-            let erate = ((egress - last_egress) * 2) as f64;
-
-            // Time elapsed
             let elapsed = application_start.elapsed();
-            let time_string = format!("{}s {}ms",elapsed.as_secs(),elapsed.subsec_millis());
+
+            // Get current counter values
+            let dropped = self.events_drops.get_rate_string(elapsed);
+            let handled = self.events_handled.get_rate_string(elapsed);
+            let ingress = self.ingress_counter.get_rate_string(elapsed);
+            let egress = self.egress_counter.get_rate_string(elapsed);
+
+            // Time elapsed display string
+            let time_string = format!("{}s {}ms", elapsed.as_secs(), elapsed.subsec_millis());
 
             let to_display = format!(
                 // \r returns cursor to beginning of line, effectively overwriting the last line
                 "\r| {} time elapsed | {} handled | {} dropped | {} events/s | {} drops/s | {} ingress packets/s | {} egress packets/s | ",
-                time_string,handled, dropped, hrate, drate,irate,erate
+                time_string,self.events_handled.get_counter_sum(), self.events_drops.get_counter_sum(), handled, dropped,ingress,egress
             );
 
             print!("{to_display}");
             let _ = io::stdout().flush();
 
-            // Store values for next loop iteration
-            last_dropped = dropped;
-            last_handled = handled;
-            last_ingress = ingress;
-            last_egress = egress;
-
             // Sleep until next calc
             sleep(Duration::from_millis(500))
         }
-
     }
 
     pub fn run(&mut self) -> () {
-
-        // Start tokio task that listens to keyboard press
-
-        // To calculate rate over multiple iterations
+        // Rate trcking for
         let mut last_dropped: u32 = 0;
         let mut last_handled: u32 = 0;
         let mut last_ingress: u32 = 0;
@@ -121,48 +221,49 @@ impl EBPFWatcher {
 
         let application_start = Instant::now();
 
-        let mut ingress_rates: Vec<(f64, f64)> = vec![(0.0,0.0)];
-        let mut egress_rates: Vec<(f64, f64)> = vec![(0.0,0.0)];
-        
+        // For plotting
+        // TODO: Add max number of entries handling!
+        let mut ingress_rates: Vec<(f64, f64)> = vec![(0.0, 0.0)];
+        let mut egress_rates: Vec<(f64, f64)> = vec![(0.0, 0.0)];
 
         while !self.token.is_cancelled() {
+            let elapsed = application_start.elapsed();
+
             // Get current counter values
-            let dropped = self.get_counter_sum(Counter::Dropped);
-            let handled = self.get_counter_sum(Counter::Handled);
-            let ingress = self.get_counter_sum(Counter::Ingress);
-            let egress = self.get_counter_sum(Counter::Egress);
+            let dropped = self.events_drops.get_rate_string(elapsed);
+            let handled = self.events_handled.get_rate_string(elapsed);
+            let ingress = self.ingress_counter.get_rate_string(elapsed);
+            let egress = self.egress_counter.get_rate_string(elapsed);
 
-            // Calculate current rate in 1/s
-            // TODO: change to be based on loop sleep duration
-            let drate = (dropped - last_dropped) * 2;
-            let hrate = (handled - last_handled) * 2;
-            let irate = ((ingress - last_ingress) * 2) as f64;
-            let erate = ((egress - last_egress) * 2) as f64;
+            // For graph plotting
+            let ingress_rate = self.ingress_counter.get_rate(elapsed);
+            let egress_rate = self.egress_counter.get_rate(elapsed);
 
-            if irate > max_rate {
-                max_rate = irate as f64;
+            if ingress_rate > max_rate {
+                max_rate = ingress_rate;
             }
 
-            if erate > max_rate {
-                max_rate = erate as f64;
+            if egress_rate > max_rate {
+                max_rate =egress_rate;
             }
 
-            // Add rates for graph 
-            ingress_rates.push(
-                (application_start.elapsed().as_secs_f64(),irate as f64)
-            );
-            egress_rates.push(
-                (application_start.elapsed().as_secs_f64(),erate as f64)
-            );
+            // Add rates for graph
+            ingress_rates.push((application_start.elapsed().as_secs_f64(), ingress_rate as f64));
+            egress_rates.push((application_start.elapsed().as_secs_f64(), egress_rate as f64));
 
             // Time elapsed
             let elapsed = application_start.elapsed();
-            let time_string = format!("{}s {}ms",elapsed.as_secs(),elapsed.subsec_millis());
+            let time_string = format!("{}s {}ms", elapsed.as_secs(), elapsed.subsec_millis());
+
+
+            // Blocks for UI
+
+
 
             // Generate Paragraph text
             let to_draw = Paragraph::new(format!(
                 "{} handled\n{} dropped\n{} events/s\n{} drops/s\n{} ingress packets/s\n{} egress packets/s\n{} time elapsed",
-                handled, dropped, hrate, drate,irate,erate, time_string
+                self.events_handled.get_counter_sum(), self.events_drops.get_counter_sum(), handled, dropped,ingress_rate,egress_rate, time_string
             ));
             // Block that contains paragraph
             let events_block = Block::bordered().borders(Borders::ALL).title("Event stats");
@@ -173,111 +274,96 @@ impl EBPFWatcher {
 
             // Rate chart labels
             let y_labels = vec![
-                
-                
                 Span::styled(
-                    format!("{}",0), 
-                    Style::default().add_modifier(Modifier::BOLD)
+                    format!("{}", 0),
+                    Style::default().add_modifier(Modifier::BOLD),
                 ),
                 Span::styled(
-                    format!("{}",max_rate/2.0), 
-                    Style::default().add_modifier(Modifier::BOLD)
+                    format!("{}", max_rate / 2.0),
+                    Style::default().add_modifier(Modifier::BOLD),
                 ),
                 Span::styled(
-                    format!("{}",max_rate), 
-                    Style::default().add_modifier(Modifier::BOLD)
+                    format!("{}", max_rate),
+                    Style::default().add_modifier(Modifier::BOLD),
                 ),
-
             ];
 
             let x_labels = vec![
                 Span::styled(
-                    format!("{}",0), 
-                    Style::default().add_modifier(Modifier::BOLD)
+                    format!("{}", 0),
+                    Style::default().add_modifier(Modifier::BOLD),
                 ),
                 Span::styled(
-                    format!("{}",elapsed.as_secs() as f64 /2.0), 
-                    Style::default().add_modifier(Modifier::BOLD)
+                    format!("{}", elapsed.as_secs() as f64 / 2.0),
+                    Style::default().add_modifier(Modifier::BOLD),
                 ),
                 Span::styled(
-                    format!("{}",elapsed.as_secs()), 
-                    Style::default().add_modifier(Modifier::BOLD)
+                    format!("{}", elapsed.as_secs()),
+                    Style::default().add_modifier(Modifier::BOLD),
                 ),
             ];
 
-
             // Rate chart
-            let chart = Chart::new(
-                vec![
-                    Dataset::default()
-                        .name("Ingress")
-                        .marker(symbols::Marker::Dot)
-                        .style(Style::default().fg(Color::Cyan))
-                        .graph_type(GraphType::Line)
-                        .data(&ingress_rates),
-                    Dataset::default()
-                        .name("Egress")
-                        .marker(symbols::Marker::Dot)
-                        .style(Style::default().fg(Color::LightGreen))
-                        .graph_type(GraphType::Line)
-                        .data(&egress_rates),
-                ]
+            let chart = Chart::new(vec![
+                Dataset::default()
+                    .name("Ingress")
+                    .marker(symbols::Marker::Dot)
+                    .style(Style::default().fg(Color::Cyan))
+                    .graph_type(GraphType::Line)
+                    .data(&ingress_rates),
+                Dataset::default()
+                    .name("Egress")
+                    .marker(symbols::Marker::Dot)
+                    .style(Style::default().fg(Color::LightGreen))
+                    .graph_type(GraphType::Line)
+                    .data(&egress_rates),
+            ])
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title("Packet Rate Graph"),
             )
-            .block(Block::default().borders(Borders::ALL).title("Packet Rate Graph"))
-            .x_axis(Axis::default()
-                .title("Time (s)")
-                .style(Style::default().fg(Color::White))
-                .bounds([0.0,elapsed.as_secs_f64()])
-                .labels(x_labels)
+            .x_axis(
+                Axis::default()
+                    .title("Time (s)")
+                    .style(Style::default().fg(Color::White))
+                    .bounds([0.0, elapsed.as_secs_f64()])
+                    .labels(x_labels),
             )
-            .y_axis(Axis::default()
-                .title("Packets/s")
-                .style(Style::default().fg(Color::White))
-                .bounds([0.0,max_rate])
-                .labels(y_labels)
+            .y_axis(
+                Axis::default()
+                    .title("Packets/s")
+                    .style(Style::default().fg(Color::White))
+                    .bounds([0.0, max_rate])
+                    .labels(y_labels),
             );
 
             // Render function
             // TODO: move to own function
             let _ = self.terminal.as_mut().unwrap().draw(|frame| {
-
                 // Main layout
-                let areas= Layout::default()
+                let areas = Layout::default()
                     .direction(Direction::Vertical)
-                    .constraints(vec![
-                        Constraint::Min(8),
-                        Constraint::Max(3)
-                    ])
+                    .constraints(vec![Constraint::Min(8), Constraint::Max(3)])
                     .split(frame.area());
 
                 // Top layout
                 let top_areas = Layout::default()
                     .direction(Direction::Horizontal)
-                    .constraints(vec![
-                        Constraint::Percentage(20),
-                        Constraint::Percentage(80)
-                    ])
+                    .constraints(vec![Constraint::Percentage(20), Constraint::Percentage(80)])
                     .split(areas[0]);
 
                 frame.render_widget(to_draw.block(events_block), top_areas[0]);
                 frame.render_widget(chart, top_areas[1]);
 
                 frame.render_widget(keybindings.block(keybindings_block), areas[1]);
-
             });
-
-            // Store values for next loop iteration
-            last_dropped = dropped;
-            last_handled = handled;
-            last_ingress = ingress;
-            last_egress = egress;
 
             // Wait for key event for 0.5s and check key presses inbetween runs
             let start = Instant::now();
 
             // Loop until 500ms elapsed
             while start.elapsed().as_millis() < 500 {
-
                 // Poll for eavent ready
                 // Timout after 10ms
                 // On Error continue to next loop iteration
@@ -291,12 +377,11 @@ impl EBPFWatcher {
                     let Ok(event::Event::Key(key)) = event::read() else {
                         continue;
                     };
-                    
+
                     // Check for esc or q to cancel
                     if key.code == KeyCode::Esc || key.code == KeyCode::Char('q') {
                         self.token.cancel();
                     }
- 
                 }
             }
         }
@@ -305,35 +390,4 @@ impl EBPFWatcher {
         ratatui::restore();
     }
 
-    fn get_counter_sum(&self, counter: Counter) -> u32 {
-        // Get counter based on passed enum
-        let counter_instance = match counter {
-            Counter::Dropped => &self.events_drops,
-            Counter::Egress => &self.egress_counter,
-            Counter::Handled => &self.events_handled,
-            Counter::Ingress => &self.ingress_counter
-        };
-
-        // Counter is per CPU, sum will hold sum across CPUs
-        let mut sum: u32 = 0;
-
-        // Get counter array
-        let values = counter_instance.get(&0, 0);
-
-        // Check if error erturned
-        match values {
-            Err(err) => {
-                error!("Failed to read event drop counters: {}!", err);
-            }
-            Ok(counters) => {
-                // Iterate and sum over array
-                for i in 1..nr_cpus().expect("failed to get number of cpus") {
-                    sum += counters[i];
-                }
-            }
-        }
-        sum
-    }
-
-    
 }
