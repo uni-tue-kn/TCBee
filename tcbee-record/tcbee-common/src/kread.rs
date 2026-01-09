ffd3e9fd1c2df7127a53fe2797e9dc89f600bfe6
Moritz Flüchter <moritz.fluechter@uni-tuebingen.de>
2026-01-09 14:00:11 +0100

    Fix missing unsafe for read_kernel func

--- tcbee-record/tcbee-common/src/kread.rs
+++ tcbee-record/tcbee-common/src/kread.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "ebpf")]
 #[inline(always)]
-pub fn read_kernel<T>(src: *const T) -> Result<T, u32> {
+pub unsafe fn read_kernel<T>(src: *const T) -> Result<T, u32> {
     // Use aya helper directly here (common gets this only in ebpf builds)
     unsafe { aya_ebpf::helpers::bpf_probe_read_kernel(src).map_err(|_| 1u32) }
 }
